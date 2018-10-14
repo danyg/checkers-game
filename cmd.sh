700124276b162c37716a95e79fb11e57cbdf31a2
danyg <dalgo86@gmail.com>
2018-10-14 18:54:44 +0200

    DG [#1] MOD: refactor cmd.sh to have its internals utils in a different file so that is easier to read

--- cmd.sh
+++ cmd.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
 source .cmd-sources.sh
-export MSYS_NO_PATHCONV=1 # needed for git bash on windows
-HELP='These are the commands supported:\n'
-GENERAL_INFO=''
-H_COLOR='\e[1;36m'
-R_COLOR='\e[0m'
-DEFAULT_DOCKERC_FILES='-f ./docker-compose.yaml'
-
-function setDevelEnv {
-	DOCKERC_FILES=$DEFAULT_DOCKERC_FILES' -f ./docker-compose.devel.yaml'
-}
-
-function checkEnv {
-	case $ENV in
-		development|devel)
-			setDevelEnv
-			;;
-		*)
-			DOCKERC_FILES=$DEFAULT_DOCKERC_FILES
-	esac
-}
-
-function fn_exists() {
-	"$(type -t ${1})" = 'function'
-}
-
-function addHelp {
-	HELP=$HELP$1'\n\n'
-}
-
-function addGI {
-	GENERAL_INFO=$GENERAL_INFO$1'\n'
-}
-
-function highlight {
-	echo $H_COLOR$1$R_COLOR
-}
-
 ###############################################################################
 # COMMANDS
 
